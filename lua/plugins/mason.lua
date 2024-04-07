cc44c739e818df571d1aa603156a48b8cbf3ae92
her14nto <her14nto@gmail.com>
2024-04-07 09:21:48 +0700

    disable lua_ls and python dap

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -9,7 +9,8 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-        "lua_ls",
+        -- "lua_ls",
+        -- "phpactor",
         -- add more arguments for adding more language servers
       })
     end,
@@ -35,7 +36,7 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-        "python",
+        -- "python",
         -- add more arguments for adding more debuggers
       })
     end,
