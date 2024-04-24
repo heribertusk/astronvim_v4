64377f3d448766a3a1b0df64cc3beaa9df901ae0
her14nto <her14nto@gmail.com>
2024-04-24 08:05:38 +0700

    disable emmet_ls formatting, make sure volar as vue file formatter

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -29,18 +29,21 @@ return {
           "ts",
         },
         ignore_filetypes = { -- disable format on save for specified filetypes
-          "python",
+          -- "python",
         },
       },
       disabled = { -- disable formatting capabilities for the listed language servers
         -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
         "lua_ls",
-        "intelephense"
+        "emmet_ls"
       },
       timeout_ms = 1000, -- default format timeout
-      -- filter = function(client) -- fully override the default formatting function
-      --   return true
-      -- end
+      filter = function(client) -- fully override the default formatting function
+        if vim.bo.filetype == "vue" then
+          return client.name == "volar"
+        end
+        return true
+      end
     },
     -- enable servers that you already have installed without mason
     servers = {
