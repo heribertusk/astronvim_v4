d59da509b1eeb728ef9274fa1eae1f4aeb8b0a7d
her14nto <her14nto@gmail.com>
2024-05-07 09:26:39 +0700

    remove some custom config after using community pack

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -4,15 +4,14 @@ return {
   config = function(plugin, opts)
     -- include the default astronvim config that calls the setup call
     require "astronvim.plugins.configs.luasnip"(plugin, opts)
-    
+
     local luasnip = require "luasnip"
     luasnip.filetype_extend("javascript", { "vue" })
     luasnip.filetype_extend("php", { "blade" })
-    
+
     -- load snippets paths
     require("luasnip.loaders.from_vscode").lazy_load {
       paths = { vim.fn.stdpath "config" .. "/snippets" },
     }
-
   end,
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -16,18 +16,5 @@ return {
       "scss",
       -- add more arguments for adding more treesitter parsers
     })
-    -- add support to laravel-blade
-    local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-
-    parser_config.blade = {
-      install_info = {
-        url = "https://github.com/EmranMR/tree-sitter-blade",
-        files = { "src/parser.c" },
-        branch = "main",
-      },
-      filetype = "blade",
-    }
-
-    opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "blade" })
   end,
 }
