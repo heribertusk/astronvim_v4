10d93d56acce9ff4fd24c4c38498f24f34e21d78
her14nto <her14nto@gmail.com>
2024-07-29 10:28:33 +0700

    handle wrapping text

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -75,4 +75,20 @@ return {
       )
     end,
   },
+  {
+    "andrewferrier/wrapping.nvim",
+    opts = {
+      auto_set_mode_filetype_allowlist = {
+        "asciidoc",
+        "gitcommit",
+        "latex",
+        "mail",
+        "markdown",
+        "rst",
+        "tex",
+        "text",
+      },
+    },
+    config = function(_, opts) require("wrapping").setup(opts) end,
+  },
 }
