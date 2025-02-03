7b5170196318b3bdde155afb4ea73d897c1d360b
her14nto <her14nto@gmail.com>
2025-02-03 07:17:09 +0700

    add ts blade support syntax

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -14,7 +14,27 @@ return {
       "typescript",
       "css",
       "scss",
+      "blade",
+      "php_only",
       -- add more arguments for adding more treesitter parsers
     })
+  end,
+  config = function(_, opts)
+    vim.filetype.add {
+      pattern = {
+        [".*%.blade%.php"] = "blade",
+      },
+    }
+
+    require("nvim-treesitter.configs").setup(opts)
+    local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+    parser_config.blade = {
+      install_info = {
+        url = "https://github.com/EmranMR/tree-sitter-blade",
+        files = { "src/parser.c" },
+        branch = "main",
+      },
+      filetype = "blade",
+    }
   end,
 }
